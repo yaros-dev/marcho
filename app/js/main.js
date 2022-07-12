acaf9a135afa6e3a7f546997028596109228a178
yaros-dev <yaroslavpazynenko@gmail.com>
2022-07-12 14:00:53 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,13 @@
 $(function () {
   
-
+  $('.top-slider__inner').slick({
+    arrows: false,
+    dots: true,
+    infinite: true,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
 
 
 })
